89f05e95ff81b8e78726f06a6854b0499b5b00f3
suneunkim <tjsdms89@gmail.com>
2023-10-31 15:13:07 +0900

    feat: 서로 다른 임의의 수 3개 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
-  async play() {}
+  constructor() {
+    this.reandomNumber = this.generateRandomNumber();
+  }
+
+  async play() {
+    console.log("숫자 야구 게임을 시작합니다");
+    await this.generateRandomNumber();
+  }
+
+  generateRandomNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+    console.log(computerNumber);
+    return computerNumber.join("");
+  }
 }
 
 export default App;
